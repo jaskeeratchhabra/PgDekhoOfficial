073816b6a687f03fe50b7151496816eaa10866a7
jaskeeratchhabra <jkchhabra99@gmail.com>
2024-02-15 12:26:30 +0530

    ignore

--- server/server.js
+++ server/server.js
@@ -10,4 +10,4 @@ const roomsRoute=require('./routes/roomsRoute')
 app.use('/api/rooms',roomsRoute)
 
 const port= process.env.PORT || 5000;
-app.listen(port,()=> (console.log(`server runnning on port ${port}`)))
+app.listen(port,()=> (console.log(`server runnning on port ${port}`))) ;
