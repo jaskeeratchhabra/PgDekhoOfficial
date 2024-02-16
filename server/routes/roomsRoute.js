cbc90c0219d0c0773c6f68a57868dd7851463070
jaskeeratchhabra <jkchhabra99@gmail.com>
2024-02-16 19:33:26 +0530

    new

--- server/routes/roomsRoute.js
+++ server/routes/roomsRoute.js
@@ -4,8 +4,9 @@ const router=express.Router();
 
 const Room=require('../models/rooms')
 
+
 router.get("/getallrooms", async(req,res)=>{
- 
+    
     try{
         const rooms=await Room.find({});
         return res.send(rooms);
@@ -13,7 +14,23 @@ router.get("/getallrooms", async(req,res)=>{
     catch(error){
         return res.status(400).json({message:error});
     }
+});
+router.post("/getroombyid", async(req,res)=>{
+    // console.log({roomid});
+ 
+    const roomid=req.body; 
+
+    try{
+        const room=await Room.findOne({_id:roomid});
+        return res.send(room);
+    }
+    catch(error){
+        console.log(error.message)
+        console.log("hey")
+        return res.status(400).json({message:error});
+    }
     
 });
+
 
 module.exports=router;
--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ const dbConfig=require('./dbSetup');
 
 const roomsRoute=require('./routes/roomsRoute')
 
+app.use(express.json());
 app.use('/api/rooms',roomsRoute)
 
 const port= process.env.PORT || 5000;
